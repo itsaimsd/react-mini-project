22a1b939629af571e795018c6f2ee9217bd1628a
itsaimsd <madhusudantiwari2003@gmail.com>
2024-12-10 10:08:23 +0530

    project part 2 is about to complete

--- Project-2/src/component/Button/Button.jsx
+++ Project-2/src/component/Button/Button.jsx
@@ -1,5 +1,11 @@
-const Button = () => {
-  return <div>Button</div>;
+import styles from "./Button.module.css";
+const Button = ({ isOutline, icon, text }) => {
+  return (
+    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+      {icon}
+      {text}
+    </button>
+  );
 };
 
 export default Button;
