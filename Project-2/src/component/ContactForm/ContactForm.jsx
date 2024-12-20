1ae064dbcd6215ebda5ac6b99ff3c02b10034aaa
itsaimsd <madhusudantiwari2003@gmail.com>
2024-12-20 20:33:48 +0530

    contact

--- Project-2/src/component/ContactForm/ContactForm.jsx
+++ Project-2/src/component/ContactForm/ContactForm.jsx
@@ -3,7 +3,7 @@ import Button from "../Button/Button";
 import { MdMessage } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-
+// This is contact form
 const ContactForm = () => {
   return (
     <section className={styles.container}>
@@ -26,9 +26,25 @@ const ContactForm = () => {
             <label htmlFor="name">Name</label>
             <input type="text" name="name" />
           </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="text">Text</label>
+            <textarea name="text" rows={8} />
+          </div>
+
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <Button text="SUBMIT BUTTON" />
+          </div>
         </form>
       </div>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="/image/Service 24_7-pana 1.svg" alt="contact image" />
+      </div>
     </section>
   );
 };
