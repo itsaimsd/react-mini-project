3f524343fcebf17c37d4a1be6bcd195be19d78b5
itsaimsd <madhusudantiwari2003@gmail.com>
2024-12-25 13:18:04 +0530

    started project 4

--- project-4/app/src/App.jsx
+++ project-4/app/src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container>
+      <TopContainer>
+        <div className="logo">
+          <img src="/Foody Zone.svg" alt="logo" />
+        </div>
+
+        <div className="search">
+          <input placeholder="Search Food" />
+        </div>
+      </TopContainer>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const TopContainer = styled.div`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between ;
+  padding: 16px;
+  align-items: center;
+`;
